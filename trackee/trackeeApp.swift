33dbe91e0c142f84fcbee488ca227540c5e36f3c
joricky91 <jonathanricky91@gmail.com>
2022-07-21 12:36:34 +0700

    Initial Commit

---
+++ trackee/trackeeApp.swift
@@ -0,0 +1,20 @@
+//
+//  trackeeApp.swift
+//  trackee
+//
+//  Created by Jonathan Ricky Sandjaja on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct trackeeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
